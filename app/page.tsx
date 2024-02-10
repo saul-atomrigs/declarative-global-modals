41e32c7683a18258bf8a570ca0eec12cad2322a5
Saul Lee <solleedata@gmail.com>
2024-02-10 19:53:59 +0900

    feat: RecoilModal

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,28 @@
 'use client';
-
 import Button from './components/Button';
 import ZustandModal from './components/ZustandModal';
+import RecoilModal from './components/RecoilModal';
 import useModalZustandStore from './hooks/useModalZustandStore';
+import useModalRecoilStore from './hooks/useModalRecoilStore';
 
 export default function Home() {
   const zustandModal = useModalZustandStore();
+  const recoilModal = useModalRecoilStore('구글가입');
 
-  const onOpenModalClick = () => {
+  const onOpenZustandModalClick = () => {
     zustandModal.open('hello!');
+  };
+
+  const onOpenRecoilModalClick = () => {
+    recoilModal.open();
   };
 
   return (
     <>
-      <Button onClick={onOpenModalClick}>Click me</Button>;
+      <Button onClick={onOpenZustandModalClick}>Open Zustand modal</Button>;
+      <Button onClick={onOpenRecoilModalClick}>Open Recoil modal</Button>
       <ZustandModal />
+      <RecoilModal modalName='구글가입' />
     </>
   );
 }
